2cd5cb70e121f319282b179bf0560bd67ac81a18
Remi Duplan <duplan.remi@gmail.com>
2025-02-17 16:56:17 +0100

    added some admin models in invitation

--- invitation_app/admin.py
+++ invitation_app/admin.py
@@ -2,3 +2,6 @@ from django.contrib import admin
 from invitation_app.models import Invitation
 # Register your models here.
 admin.site.register(Invitation)
+admin.site.register(ProposedDate)
+admin.site.register(InvitationNote)
+admin.site.register(InvitationStatus)
