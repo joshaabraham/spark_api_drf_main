cb1a50205638c38ea3b9ee756f4a3fa402eaa045
Remi Duplan <duplan.remi@gmail.com>
2025-03-10 12:31:37 +0100

    Model de filtre

--- sport_events_app/api/views.py
+++ sport_events_app/api/views.py
@@ -9,6 +9,23 @@ from .serializers import (
     LocationSerializer, MatchSerializer, SportEventSerializer
 )
 
+class SportEventFilterView(generics.ListAPIView):
+    queryset = SportEvent.objects.all()
+    serializer_class = SportEventSerializer
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_fields = ['sport_type', 'geographic_scope', 'frequency', 'objective']
+    ordering_fields = ['start_date', 'estimated_audience']
+    ordering = ['start_date']  # Default ordering
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        user = self.request.user
+        # Filter by sports practiced by the user
+        if user.is_authenticated:
+            user_sports = user.profile.sports.all()
+            queryset = queryset.filter(sports__in=user_sports).distinct()
+        return queryset
+
 class GeoCoordinatesListCreateView(generics.ListCreateAPIView):
     queryset = GeoCoordinates.objects.all()
     serializer_class = GeoCoordinatesSerializer
