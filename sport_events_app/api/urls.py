8e977df1de801554ff85c2462d7d802fa53118a9
Remi Duplan <duplan.remi@gmail.com>
2025-03-28 11:40:07 +0100

    modif player app

--- sport_events_app/api/urls.py
+++ sport_events_app/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import (
-    GeoCoordinatesListCreateView, GeoCoordinatesDetailView,
+    GeoCoordinatesListCreateView, GeoCoordinatesDetailView, SportEventFilterView,
     SportListCreateView, SportDetailView,
     PlayerListCreateView, PlayerDetailView,
     TeamListCreateView, TeamDetailView,
